1e9f23c92799d6771ce27ba3a85bf69e834dce92
Juha Järvinen <juha.3.jarvinen@tuni.fi>
2022-12-06 16:06:01 +0200

    User struct and loginpage created

--- src/structs/user.rs
+++ src/structs/user.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use serde::{Deserialize, Serialize};
 
 /// Define fields for user. Used for creating and logging users.
@@ -7,7 +8,30 @@ pub struct User {
     pub username: String,
     pub email: String,
     // Only used to take input when loggin in or creating a user. Not used to save password variable on client side!
+=======
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct User {
+    pub username: String,
+    pub email: String,
+>>>>>>> 52bf42c (User struct and loginpage created)
     pub password: String,
     pub token: String,
     pub is_logged_in: bool,
 }
+<<<<<<< HEAD
+=======
+
+impl Default for User {
+    fn default() -> Self {
+        Self {
+            username: "".to_owned(),
+            email: "".to_owned(),
+            password: "".to_owned(),
+            token: "".to_owned(),
+            is_logged_in: false,
+        }
+    }
+}
+>>>>>>> 52bf42c (User struct and loginpage created)
